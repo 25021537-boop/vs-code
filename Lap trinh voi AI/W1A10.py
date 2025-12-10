98af5d8225f9254e4e807ddb48cc50ec1bd79650
25021537-boop <25021537@vnu.edu.vn>
2025-12-10 09:34:02 +0700

    a

---
+++ Lap trinh voi AI/W1A10.py
@@ -0,0 +1,6 @@
+# Simple menu
+print("=== AI Prediction system===")
+print("1,sentiment analysis")
+print("2,weather forecasst")
+print('3,exit')
+print("Pplease choose an option:")
---
+++ Lap trinh voi AI/W1A9.py
@@ -0,0 +1,2 @@
+for x in ["cat","doc","fish"]:
+    print("Dự đoán con vật:",x)
